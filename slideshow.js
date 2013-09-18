c27f23b9502b1f8b6dffd31743b0733266749959
mtigue <mtigue@web4uonline.com>
2013-09-18 08:22:44 -0400

    1

---
+++ slideshow.js
@@ -0,0 +1,39 @@
+﻿document.write("<!-----for external home---->	");
+document.write("	<div data-role=\"content\">");
+document.write("		");
+document.write("<!-- slider -->");
+document.write("    <div class=\"slider-outer\">");
+document.write("    	<div class=\"slider-container\">");
+document.write("			<div id=\"camera_wrap\">		");
+document.write("		");
+document.write("		 ");
+document.write("<div data-src='slideshow\/memorial.jpg'>");
+document.write("<div class='camera_caption fadeFromRight font'>Welcome to the Catholic Community at Stanford<\/div>");
+document.write("<\/div>");
+document.write("<div data-src='slideshow\/dome.jpg'>");
+document.write("<div class='camera_caption fadeFromRight'>Memorial Church<\/div>");
+document.write("");
+document.write("<\/div>");
+
+document.write("");
+document.write("");
+document.write("");
+document.write("<div data-src='slideshow\/ocean.jpg'>");
+// document.write("<div classx='camera_caption fadeFromRight'><\/div>");
+document.write("<\/div>");
+document.write("");
+
+document.write("<div data-src='slideshow\/lamps.jpg'>");
+// document.write("<div class='camera_caption fadeFromRight'>The Memorial Chapel<\/div>");
+document.write("<\/div>");
+
+
+document.write("	");
+document.write(" ");
+document.write(" ");
+document.write("            ");
+document.write("     		<\/div><!--end camera-wrap-->");
+document.write("		<\/div><!---end slider container-->");
+document.write("        <br class=\"clear\" \/>");
+document.write("	<\/div><!-- \/slider -->  ");
+document.write("");
---
+++ web4u.js
@@ -0,0 +1,4 @@
+﻿function refresh() {
+    window.location='index.html';
+
+}
